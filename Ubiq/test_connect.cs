661f4e5c88a5c54d52735990768c17cdb3668ef6
csMiz <38314406+csMiz@users.noreply.github.com>
2021-09-15 11:58:19 +0800

    archive

--- Ubiq/test_connect.cs
+++ Ubiq/test_connect.cs
@@ -10,14 +10,17 @@ public class test_connect : MonoBehaviour
 
     private System.Net.Sockets.Socket TCPListener = new System.Net.Sockets.Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
 
-    private List<float> receivedData = new List<float>();
+    private List<float[]> receivedData = new List<float[]>();
+    private object syncLock = new object();
 
+    private SkinnedMeshRenderer skinnedMeshRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        //StartListener();
+        skinnedMeshRenderer = this.GetComponent<SkinnedMeshRenderer>();
 
+        // start listener
         Thread thread = new Thread(new ThreadStart(ThreadListen));
         thread.Start();
 
@@ -27,43 +30,53 @@ public class test_connect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //    byte[] bytes;
-        //    System.Net.Sockets.Socket handler = TCPListener.Accept();
-        //    byte[] poseData = new byte[4];
-
-        //    bytes = new byte[1024];
-        //    int bytesRec = handler.Receive(bytes);    //可能会卡在这
-        //    if (bytesRec > 0) {
-        //        poseData[0] = bytes[0];
-        //        poseData[1] = bytes[1];
-        //        poseData[2] = bytes[2];
-        //        poseData[3] = bytes[3];
-        //    }
-        //    else{
-        //    }
-
-        //    handler.Shutdown(System.Net.Sockets.SocketShutdown.Both);
-        //    handler.Close();
-        //    handler.Dispose();
-
-        //    if (poseData[0] == 0)
-        //    {
-        //        gameObject.transform.localScale = new Vector3(1, 1, 1);
-
-        //    }
-        //    else {
-        //        gameObject.transform.localScale = new Vector3(0.1f * Convert.ToSingle(poseData[0]), 1, 1);
-        //    }
 
         if (receivedData.Count > 0) {
-            gameObject.transform.localScale = new Vector3(receivedData[0], 1, 1);
+            // optional: filtering
+            // float[] dataIn = ApplyLowPassFilter(receivedData, kernelSize);
+            // float[] dataIn = ApplyGaussianFilter(receivedData, kernelSize, sigma);
+
+            float[] dataIn = receivedData[receivedData.Count - 1];
+
+            // apply animation
+            float v1 = 100f * dataIn[0];
+            float v2 = 100f * dataIn[1];
+            float v3 = 100f * dataIn[2];
+            if (v1 < 0) { v1 = 0; }
+            if (v2 < 0) { v2 = 0; }
+            if (v3 < 0) { v3 = 0; }
+            if (v1 > 100) { v1 = 100; }
+            if (v2 > 100) { v2 = 100; }
+            if (v3 > 100) { v3 = 100; }
+
+            // smile
+            skinnedMeshRenderer.SetBlendShapeWeight(39, v1);    // smile left
+            skinnedMeshRenderer.SetBlendShapeWeight(40, v1);    // smile right
+            skinnedMeshRenderer.SetBlendShapeWeight(41, v1);    // eye smile left
+            skinnedMeshRenderer.SetBlendShapeWeight(42, v1);    // eye smile right
+
+            // sad
+            skinnedMeshRenderer.SetBlendShapeWeight(13, v2);    // frown left
+            skinnedMeshRenderer.SetBlendShapeWeight(14, v2);    // frown right
+            skinnedMeshRenderer.SetBlendShapeWeight(30, v2);    // mouth down
+
+            // surprise
+            skinnedMeshRenderer.SetBlendShapeWeight(33, v3);    // mouth open
+
+            if (receivedData.Count > 15) {    // clean buffer if buffer size is too large
+                lock (syncLock) {
+                    while (receivedData.Count > 15) {
+                        receivedData.RemoveAt(0);
+                    }
+                }
+            }
         }
 
     }
 
     public void StartListener()
     {
-        System.Net.IPEndPoint localEndPoint = new System.Net.IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+        System.Net.IPEndPoint localEndPoint = new System.Net.IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);    // listen at localhost, port 8888
         TCPListener.Bind(localEndPoint);
         TCPListener.Listen(10);
 
@@ -72,29 +85,22 @@ public class test_connect : MonoBehaviour
     }
 
     public void ThreadListen() {
-        //  程序结束时不会终止此线程
-        int count = 0;
-
+        
         StartListener();
 
-
-        while (count < 10) {
-
-            DateTime st = DateTime.Now;
+        while (true) {    // listening tcp connection
 
             byte[] bytes;
             System.Net.Sockets.Socket handler = TCPListener.Accept();
-            byte[] poseData = new byte[4];
+            byte[] poseData = new byte[3];
 
             bytes = new byte[1024];
             int bytesRec = handler.Receive(bytes); 
             if (bytesRec > 0)
             {
-                poseData[0] = bytes[0];
-                poseData[1] = bytes[1];
-                poseData[2] = bytes[2];
-                poseData[3] = bytes[3];
-                Debug.Log(poseData[0]);
+                for (int i = 0; i < 3; i++) {    // smile, sad and surprise values
+                    poseData[i] = bytes[i];
+                }
             }
             else
             {
@@ -104,25 +110,21 @@ public class test_connect : MonoBehaviour
             handler.Close();
             handler.Dispose();
 
-            if (poseData[0] == 0)
+            if (poseData[0] != 0)
             {
-            }
-            else
-            {
-                //gameObject.transform.localScale = new Vector3(0.1f * Convert.ToSingle(poseData[0]), 1, 1);   //需要回到原始线程
-                receivedData.Clear();
-                receivedData.Add(0.1f * Convert.ToSingle(poseData[0]));
+                // send back to original thread
+                float[] content = new float[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    content[i] = Convert.ToSingle(poseData[i]);
+                }
 
-            }
-
-
-            DateTime ed = DateTime.Now;
-            while ((ed - st).TotalMilliseconds < 1000)
-            {
-                ed = DateTime.Now;
+                lock(syncLock)
+                {
+                    receivedData.Add(content);
+                }
             }
 
-            count++;
         }
 
 
